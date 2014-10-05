7d1792969cec3e5de6d38cbad024f7cc86b8badc
rinukkusu <rinukkusu@sub-r.de>
2014-10-05 23:36:36 +0200

    renamed Server member in Message

--- Shinkei/IRC/Message.cs
+++ Shinkei/IRC/Message.cs
@@ -8,14 +8,14 @@ namespace Shinkei.IRC
 {
     public class Message
     {
+        public Server ServerInstance;
         public IEntity Sender;
         public IEntity Recipient;
-        public Server Server;
         public string Text;
 
         public Message(Server _Server, IEntity _Sender, IEntity _Recipient, string _Text)
         {
-            Server = _Server;
+            ServerInstance = _Server;
             Sender = _Sender;
             Recipient = _Recipient;
             Text = _Text;
