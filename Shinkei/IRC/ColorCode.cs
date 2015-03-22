0336d0663dff56d646e143bba2a32742bcc0d9d4
Trojaner_ <trojaner25@gmail.com>
2015-03-22 20:35:34 +0200

    Some smaller changes to ColorCodes

---
+++ Shinkei/IRC/ColorCode.cs
@@ -0,0 +1,30 @@
+﻿namespace Shinkei.IRC
+{
+    public class ColorCode
+    {
+        public static string WHITE = COLOR + "00";
+        public static string BLACK = COLOR + "01";
+        public static string DARK_BLUE = COLOR + "02";
+        public static string DARK_GREEN = COLOR + "03";
+        public static string RED = COLOR + "04";
+        public static string BROWN = COLOR + "05";
+        public static string PURPLE = COLOR + "06";
+        public static string OLIVE = COLOR + "07";
+        public static string YELLOW = COLOR + "08";
+        public static string GREEN = COLOR + "09";
+        public static string TEAL = COLOR + "10";
+        public static string CYAN = COLOR + "11";
+        public static string BLUE = COLOR + "12";
+        public static string MAGENTA = COLOR + "13";
+        public static string DARK_GRAY = COLOR + "14";
+        public static string LIGHT_GRAY = COLOR + "15";
+        public static char COLOR = (char)3;
+        public static char NORMAL = (char)15;
+        public static char BOLD = (char)2;
+        public static char ITALIC = (char)9;
+        public static char STRIKETROUGH = (char)19;
+        public static char UNDERLINE = (char)21;
+        public static char UNDERLINE_ALT = (char)31;
+        public static char REVERSE = (char)22;
+    }
+}
